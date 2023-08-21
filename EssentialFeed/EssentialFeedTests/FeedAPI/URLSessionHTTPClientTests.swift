f8e3bd60ef8dd480c7b79c5d5c230fc0b7a3b9af
Dominique <dominique.bezerra@ifood.com.br>
2023-08-21 17:39:34 -0300

    Delivers unexpected values error when request completes with data, response and error as nil

--- EssentialFeed/EssentialFeedTests/FeedAPI/URLSessionHTTPClientTests.swift
+++ EssentialFeed/EssentialFeedTests/FeedAPI/URLSessionHTTPClientTests.swift
@@ -15,10 +15,14 @@ class URLSessionHTTPClient {
         self.session = session
     }
     
+    struct UnexpectedValuesRepresentation: Error { }
+    
     func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void) {
         session.dataTask(with: url) { _, _, error in
             if let error = error {
                 completion(.failure(error))
+            } else {
+                completion(.failure(UnexpectedValuesRepresentation()))
             }
             
         }.resume()
@@ -26,7 +30,6 @@ class URLSessionHTTPClient {
 }
 
 class URLSessionHTTPClientTests: XCTestCase {
-    
     override class func setUp() {
         URLProtocolStub.startInterceptingRequests()
     }
@@ -53,7 +56,7 @@ class URLSessionHTTPClientTests: XCTestCase {
     func  test_getFromURL_failsOnRequestError() {
         let requestError = NSError(domain: "any error", code: 1)
         URLProtocolStub.stub(data: nil, response: nil, error: requestError)
-        
+
         let exp = expectation(description: "Wait for completion")
         makeSUT().get(from: anyURL()) { result in
             switch result {
@@ -64,6 +67,22 @@ class URLSessionHTTPClientTests: XCTestCase {
                 XCTAssertNotNil(receivedError)
             default:
                 XCTFail("Expected failure with error \(requestError), got \(result) instead")
+            }
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 1.0)
+    }
+    
+    func  test_getFromURL_failsOnAllNilValues() {
+        URLProtocolStub.stub(data: nil, response: nil, error: nil)
+        
+        let exp = expectation(description: "Wait for completion")
+        makeSUT().get(from: anyURL()) { result in
+            switch result {
+            case .failure:
+                break
+            default:
+                XCTFail("Expected failure, got \(result) instead")
             }
             exp.fulfill()
         }
