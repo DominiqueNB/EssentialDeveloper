7ffdc35110a96f7e4c2ed852f5330a14be604e4b
Dominique <dominique.bezerra@ifood.com.br>
2023-09-07 12:27:30 -0300

    Move 'LocalFeedLoader' and 'FeedStore' collaborator to its own file in production

---
+++ EssentialFeed/EssentialFeed/FeedCache/FeedStore.swift
@@ -0,0 +1,16 @@
+//
+//  LocalFeedLoader.swift
+//  EssentialFeed
+//
+//  Created by Dominique Bezerra on 07/09/23.
+//
+
+import Foundation
+
+public protocol FeedStore {
+    typealias DeletionCompletion = (Error?) -> Void
+    typealias InsertionCompletion = (Error?) -> Void
+    
+    func deleteCachedFeed(completion: @escaping DeletionCompletion)
+    func insert(_ items: [FeedItem], timestamp: Date, completion: @escaping InsertionCompletion)
+}
