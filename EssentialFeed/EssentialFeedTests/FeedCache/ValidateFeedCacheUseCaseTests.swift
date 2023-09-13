ac92452169255c2cf20cdc1d28bfa120784cbdb7
Dominique <dominique.bezerra@ifood.com.br>
2023-09-13 18:30:35 -0300

    Extract cache deletion side effect from the 'load' method to the 'validateCache' method

--- EssentialFeed/EssentialFeedTests/FeedCache/ValidateFeedCacheUseCaseTests.swift
+++ EssentialFeed/EssentialFeedTests/FeedCache/ValidateFeedCacheUseCaseTests.swift
@@ -16,6 +16,16 @@ final class ValidateFeedCacheUseCaseTests: XCTestCase {
         XCTAssertEqual(store.receivedMessages, [])
     }
     
+    func test_validateCache_deletesCacheOnRetrievalError()
+    {
+        let (sut, store) = makeSUT()
+        
+        sut.validateCache()
+        store.completeRetrieval(with: anyNSError())
+        
+        XCTAssertEqual(store.receivedMessages, [.retrieve, .deleteCachedFeed])
+    }
+    
     // MARK: - Helpers
     private func makeSUT(currentDate: @escaping () -> Date = Date.init,
                          file: StaticString = #file,
@@ -25,5 +35,9 @@ final class ValidateFeedCacheUseCaseTests: XCTestCase {
         trackForMemoryLeaks(sut, file: file, line: line)
         trackForMemoryLeaks(store, file: file, line: line)
         return (sut, store)
+    }
+    
+    private func anyNSError() -> Error {
+        NSError(domain: "any error", code: 0)
     }
 }
