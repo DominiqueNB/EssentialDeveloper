f7d5ab06515b8cc7772b07e579b8c2d3fc6c15b0
Dominique <67060136+DominiqueNB@users.noreply.github.com>
2023-10-16 19:14:54 -0300

    Add 'EssentialFeediOS' framework (prod and test) targets for iOS platform-specific components

---
+++ EssentialFeed/EssentialFeediOSTests/EssentialFeediOSTests.swift
@@ -0,0 +1,12 @@
+//
+//  EssentialFeediOSTests.swift
+//  EssentialFeediOSTests
+//
+//  Created by Dominique Bezerra on 16/10/23.
+//
+
+import XCTest
+@testable import EssentialFeediOS
+
+final class EssentialFeediOSTests: XCTestCase {
+}
